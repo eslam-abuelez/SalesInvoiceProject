734a59d87c69a01fe637ec0efe519119c74b8859
eslam-abuelez <105518287+eslam-abuelez@users.noreply.github.com>
2022-10-28 05:47:57 +0200

    Signed-off-by: eslam-abuelez <105518287+eslam-abuelez@users.noreply.github.com>

---
+++ src/test/java/OpenBrowser.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.http.WebSocket;
+
+public class OpenBrowser {
+    public static void main(String[] args) {
+
+        //Create an object
+
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
